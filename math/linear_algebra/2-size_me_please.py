3e8ba629e61605436f8d5a9a13224061133bf8b1
otalorajuand <otalorajuand@gmail.com>
2023-03-20 20:55:18 +0000

    Addition of task 2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+"""This module containts the function matrix_shape"""
 
 
 def matrix_shape(matrix):
-
+    """This function calculates the dimesions of a matrix and 
+        returns it in a list of integers"""
     res = []
     block = matrix
     while True:
