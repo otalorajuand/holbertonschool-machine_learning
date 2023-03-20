5de63f3b2d199018eaf1b656d30682fd84d4d3f6
otalorajuand <otalorajuand@gmail.com>
2023-03-20 20:53:47 +0000

    Addition of task 2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python3
+
+
 def matrix_shape(matrix):
-    
+
     res = []
     block = matrix
     while True:
         res.append(len(block))
         block = block[0]
-        if type(block) is not list:
+        if not isinstance(block, list):
             break
     return res
