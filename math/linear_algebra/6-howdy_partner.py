1e04d66a389ec24c6a50e247b5429cf98b12be9e
otalorajuand <otalorajuand@gmail.com>
2023-03-20 21:30:27 +0000

    Addition of task 6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""this module contains the function cat_arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """this functions concatenates two arrays"""
+    return [*arr1, *arr2]
