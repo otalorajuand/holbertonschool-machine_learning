678806703c727fb5d8bfc5f27b34e03c94914625
otalorajuand <otalorajuand@gmail.com>
2023-03-21 16:35:47 +0000

    Addition of task 12

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module contains the function np_elementwise"""
+
+
+def np_elementwise(mat1, mat2):
+    """This function calculates the element-wise
+    basic operations between two arrays"""
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
