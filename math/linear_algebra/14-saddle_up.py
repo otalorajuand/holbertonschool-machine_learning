b86bf3c9a0e2f471672d8dbebdfa81e280d1b33b
otalorajuand <otalorajuand@gmail.com>
2023-03-21 16:42:43 +0000

    Addition of task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module containts the function np_matmul"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """This function multiplies two matrices"""
+    return np.matmul(mat1, mat2)
