45eddd324e4148a20b6204376282bf1ffe3889cb
otalorajuand <otalorajuand@gmail.com>
2023-03-21 16:28:35 +0000

    Addition of task 11

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains the function np_transpose"""
+
+
+def np_transpose(matrix):
+    """This function transposes a matrix"""
+    return matrix.transpose()
