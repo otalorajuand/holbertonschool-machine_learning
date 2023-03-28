2c28ec0456439d16a2715cb21a49ff1719809ac3
otalorajuand <otalorajuand@gmail.com>
2023-03-28 16:25:27 -0500

    Addition of task 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,6 +5,6 @@
 def summation_i_squared(n):
     """This function calculates the sum of the numbers squared
     until n"""
-    if n.isnumeric():
+    if not isinstance(n, int) and not isinstance(n, float):
         return None
     return int((n * (n + 1) * (2 * n + 1)) / 6)
