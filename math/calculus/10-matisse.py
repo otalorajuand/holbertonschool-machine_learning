060b91ef9fe107ee7e2df61db03ccb2d4323116f
otalorajuand <otalorajuand@gmail.com>
2023-03-28 14:51:39 -0500

    Addition of task 10

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This module contains the function poly_derivative"""
+
+
+def poly_derivative(poly):
+    """This function calculates the derivative of poly"""
+    if poly == 0:
+        return [0]
+    if not isinstance(poly, list):
+        return None
+    return [(index + 1) * elem for index, elem in enumerate(poly[1:])]
--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,6 +5,6 @@
 def summation_i_squared(n):
     """This function calculates the sum of the numbers squared
     until n"""
-    if type(n) not in [float, int]:
+    if n.isnumeric():
         return None
     return int((n * (n + 1) * (2 * n + 1)) / 6)
