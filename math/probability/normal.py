be9e89ea7491525d73c7d87c452c1d726a9f93ac
otalorajuand <otalorajuand@gmail.com>
2023-04-03 13:57:05 -0500

    Addition of task 7

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -20,3 +20,11 @@ class Normal:
             self.mean = float(sum(data) / len(data))
             self.stddev = (sum((x - (sum(data) / len(data))) **
                            2 for x in data) / len(data))**0.5
+
+    def z_score(self, x):
+        """Calculates the z-score of a given x-value"""
+        return (x - self.mean)/self.stddev
+
+    def x_value(self, z):
+        """Calculates the x-value of a given z-score"""
+        return (z * self.stddev) + self.mean
