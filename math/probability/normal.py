cc1cef332d3dd33b711a3b64f189ff2b3afb2c72
otalorajuand <otalorajuand@gmail.com>
2023-04-03 13:49:24 -0500

    Addition of task 6

---
+++ math/probability/normal.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""This module contains the class Normal"""
+
+
+class Normal:
+    """This class represents a normal distribution"""
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            else:
+                self.mean = float(mean)
+                self.stddev = float(stddev)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = float(sum(data) / len(data))
+            self.stddev = (sum((x - (sum(data) / len(data))) **
+                           2 for x in data) / len(data))**0.5
