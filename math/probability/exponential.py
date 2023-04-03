7a2292a575cdbb6278ee8d50bf9d4ce932580a16
otalorajuand <otalorajuand@gmail.com>
2023-04-03 13:25:16 -0500

    Addition of task 4

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -17,3 +17,11 @@ class Exponential:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = 1 / float(sum(data) / len(data))
+
+    def pdf(self, x):
+        """This method Calculates the value of the PDF
+        for a given time period"""
+        e = 2.7182818285
+        if x <= 0:
+            return 0
+        return self.lambtha * e**(-self.lambtha * x)
