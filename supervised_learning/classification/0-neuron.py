56e9e79452fff025779328bc562b28b0ae1f78c6
otalorajuand <otalorajuand@gmail.com>
2023-04-10 14:28:05 -0500

    Addition of task 0

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""This module contains the class Neuron"""
+import numpy as np
+
+
+class Neuron:
+    """This class defines a Neuron"""
+    def __init__(self, nx):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.W = np.random.normal(0, 1, size=(1, nx))
+        self.b = 0
+        self.A = 0
