132d0921f0b5cb67d2a51fa8782a1aa3fad93634
Otalorajuand <otalorajuand@gmail.com>
2023-04-11 17:27:06 -0500

    Addition of task 11

---
+++ supervised_learning/classification/11-neural_network.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""This module includes the Class NeuralNetwork"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """this class defines a neural network with one hidden layer
+    performing binary classification"""
+
+    def __init__(self, nx, nodes):
+
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        if not isinstance(nodes, int):
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+
+        self.__W1 = np.random.normal(0, 1, size=(nodes, nx))
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.normal(0, 1, size=(1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
+
+    def forward_prop(self, X):
+        """This function Calculates the forward propagation
+        of the neural network"""
+        Z1 = np.dot(self.W1, X) + self.b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+
+        Z2 = np.dot(self.W2, self.A1) + self.b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """Calculates the cost of the model using neural network"""
+        loss = Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        cost = -(np.sum(loss) / loss.shape[1])
+        return cost
+
