518cf9a1fa3900fcf7daf77eb7b48007043ad6c0
Otalorajuand <otalorajuand@gmail.com>
2023-04-11 16:04:32 -0500

    Addition of task 9

---
+++ supervised_learning/classification/9-neural_network.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""This module includes the Class NeuralNetwork"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """this class defines a neural network with one hidden layer
+    performing binary classification"""
+
+    def __init__(self, nx, nodes):
+
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        if not isinstance(nodes, int):
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+
+        self.__W1 = np.random.normal(0, 1, size=(nodes, nx))
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.normal(0, 1, size=(1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
