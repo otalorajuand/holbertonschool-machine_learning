1bbb5a7d5b6c4ece5b45d8ead10774877bc02d17
Otalorajuand <otalorajuand@gmail.com>
2023-04-12 14:02:07 -0500

    Addition of task 16

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""This module includes the class DeepNeuralNetwork"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """defines a deep neural network performing binary classification"""
+
+    def __init__(self, nx, layers):
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        if not isinstance(layers, list) or layers == []:
+            raise TypeError('layers must be a list of positive integers')
+        if [x for x in layers if x <= 0] != []:
+            raise ValueError('layers must be a list of positive integers')
+
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        for i, l in enumerate(layers):
+            if i == 0:
+                self.weights['W1'] = np.random.normal(0, 1, size=(l, nx))
+            else:
+                self.weights[f'W{i+1}'] = np.random.normal(
+                    0, 1, size=(l, layers[i - 1]))
+            self.weights[f'b{i+1}'] = np.zeros((l, 1))
