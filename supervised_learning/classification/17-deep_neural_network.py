81e681d8d30f936583d4773f5df592b18254d8f3
Otalorajuand <otalorajuand@gmail.com>
2023-04-12 14:53:49 -0500

    Addition of task 17

---
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""This module includes the class DeepNeuralNetwork"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """defines a deep neural network performing binary classification"""
+
+    def __init__(self, nx, layers):
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        if not isinstance(layers, list) or layers == []:
+            raise TypeError('layers must be a list of positive integers')
+        if list(filter(lambda x: x <= 0, layers)) != []:
+            raise TypeError('layers must be a list of positive integers')
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        @property
+        def L(self):
+            return self.__L
+
+        @property
+        def cache(self):
+            return self.__cache
+
+        @property
+        def weights(self):
+            return self.__weights
+
+        for i in range(len(layers)):
+            if i == 0:
+                self.__weights['W1'] = np.random.randn(
+                    layers[i], nx) * np.sqrt(2 / nx)
+            else:
+                self.__weights["W" + str(i + 1)] = np.random.randn(
+                    layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+            self.__weights["b" + str(i + 1)] = np.zeros((layers[i], 1))
