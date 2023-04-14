60869a7c07fb2dcb87582c08b387bd776b64b7c4
Otalorajuand <otalorajuand@gmail.com>
2023-04-14 12:43:37 -0500

    Addition of task 24

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -5,8 +5,9 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """This function converts a numeric label vector into a one-hot matrix"""
-    if max(Y) > classes or not isinstance(Y, np.ndarray) or
-    type(classes) is not int:
+    if max(Y) > classes or not isinstance(
+            Y, np.ndarray) or not isinstance(
+            classes, int):
         return None
 
     b = np.zeros((Y.size, Y.max() + 1))
