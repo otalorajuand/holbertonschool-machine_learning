27d8e40ce1c6f5b6ff3778c43e81a8b752120c77
Otalorajuand <otalorajuand@gmail.com>
2023-04-14 14:55:14 -0500

    Addition of task 25

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -5,5 +5,8 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """converts a one-hot matrix into a vector of labels"""
+    if not isinstance(one_hot, np.ndarray):
+        return None
+
 
     return np.argmax(one_hot, axis=0)
