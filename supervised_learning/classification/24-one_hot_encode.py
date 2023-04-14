c3f29c935d0bf4e68032959d8bbe43b7b38f7acf
Otalorajuand <otalorajuand@gmail.com>
2023-04-14 12:15:42 -0500

    Addition of task 24

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This module includes the function one_hot_encode"""
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """This function converts a numeric label vector into a one-hot matrix"""
+    a = np.array(Y)
+    b = np.zeros((a.size, a.max() + 1))
+    b[np.arange(a.size), a] = 1
+    return b.T
