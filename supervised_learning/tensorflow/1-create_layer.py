19aab0cb3000a84857b937fbc17d297b51e716d2
otalorajuand <otalorajuand@gmail.com>
2023-04-18 09:54:34 -0500

    Addition of task 1

---
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""This module contains the function create_layer"""
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    """this function creates the nn layer"""
+    with tf.name_scope("layer"):
+        layer = tf.layers.dense(
+            prev,
+            activation=activation,
+            units=n,
+            name="Tanh",
+            kernel_initializer=tf.contrib.layers.variance_scaling_initializer(
+                mode="FAN_AVG"))
+
+    return layer
