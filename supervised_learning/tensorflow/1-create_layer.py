8a4177cc267677bc06b0ca8a4acba3e0538e3b2b
otalorajuand <otalorajuand@gmail.com>
2023-04-18 09:59:41 -0500

    Addition of task 1

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -6,20 +6,10 @@ import tensorflow as tf
 def create_layer(prev, n, activation):
     """this function creates the nn layer"""
     with tf.name_scope("layer"):
-        if activation == tf.nn.tanh:
-            name = 'tanh'
-        else if activation == tf.nn.relu:
-            name = 'relu'
-        else if activation == tf.nn.sigmoid:
-            name = 'sigmoid'
-        else:
-            name = ''
-
         layer = tf.layers.dense(
             prev,
             activation=activation,
             units=n,
-            name=name,
             kernel_initializer=tf.contrib.layers.variance_scaling_initializer(
                 mode="FAN_AVG"))
 
