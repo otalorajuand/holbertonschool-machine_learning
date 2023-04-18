cd0a483f4672657c337a97bf542f1c3bfb2d6850
otalorajuand <otalorajuand@gmail.com>
2023-04-18 11:44:26 -0500

    Addition of task 4

--- supervised_learning/tensorflow/4-calculate_loss.py
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """This module contains the function calculate_loss"""
+import tensorflow as tf
 
 
 def calculate_loss(y, y_pred):
     """calculates the softmax cross-entropy loss of a prediction"""
+    y_pred = tf.math.argmax(y_pred, axis=1)
+    y = tf.math.argmax(y, axis=1)
     return tf.losses.softmax_cross_entropy(y, y_pred)
