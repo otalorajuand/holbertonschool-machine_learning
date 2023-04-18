2307377a51100eb94e3ab0c24051ff01f2209d7d
otalorajuand <otalorajuand@gmail.com>
2023-04-18 11:43:17 -0500

    Addition of task 4

---
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains the function calculate_loss"""
+
+
+def calculate_loss(y, y_pred):
+    """calculates the softmax cross-entropy loss of a prediction"""
+    return tf.losses.softmax_cross_entropy(y, y_pred)
