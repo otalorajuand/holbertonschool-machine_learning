1903dfe22eabe37bf3e4256ac3d87a32883a0d4a
otalorajuand <otalorajuand@gmail.com>
2023-04-18 11:22:35 -0500

    Addition of task 3

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -6,6 +6,6 @@ import tensorflow as tf
 def calculate_accuracy(y, y_pred):
     """calculates the accuracy of a prediction"""
     with tf.name_scope("layer"):
-        acc = tf.metrics.accuracy(labels=tf.argmax(y, 1),
+        acc, acc_op = tf.metrics.accuracy(labels=tf.argmax(y, 1),
                                   predictions=tf.argmax(y_pred, 1))
-    return acc
+    return acc_op
