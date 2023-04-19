b26ee3215de47158cdac08b041ac78056d216074
otalorajuand <otalorajuand@gmail.com>
2023-04-19 10:16:02 -0500

    Addition of task 6

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -21,10 +21,16 @@ def train(
         save_path="/tmp/model.ckpt"):
     """builds, trains, and saves a neural network classifier"""
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-    y_pred = forward_prop(X_train, layer_sizes, activations)
-    loss = calculate_loss(y, y_pred)
+    tf.add_to_collection('x', x)
+    tf.add_to_collection('y', y)
+    y_pred = forward_prop(x, layer_sizes, activations)
+    tf.add_to_collection('y_pred', y_pred)
     accuracy = calculate_accuracy(y, y_pred)
+    tf.add_to_collection('accuracy', accuracy)
+    loss = calculate_loss(y, y_pred)
+    tf.add_to_collection('loss', loss)
     train_op = create_train_op(loss, alpha)
+    tf.add_to_collection('train_op', train_op)
 
     saver = tf.train.Saver()
     init = tf.global_variables_initializer()
@@ -41,7 +47,7 @@ def train(
             accuracy_valid = sess.run(accuracy,
                                       feed_dict={x: X_valid, y: Y_valid})
 
-            if (i % 100) is 0:
+            if (i % 100) == 0:
                 print('After {} iterations:'.format(i))
                 print("\tTraining Cost: {}".format(loss_train))
                 print("\tTraining Accuracy: {}".format(accuracy_train))
