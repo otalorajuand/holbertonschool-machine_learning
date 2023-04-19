3ae00549ae926f2dfbee64d9058b4feda1a91f3b
otalorajuand <otalorajuand@gmail.com>
2023-04-19 11:07:26 -0500

    Addition of task 7

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -9,8 +9,13 @@ def evaluate(X, Y, save_path):
         saver = tf.train.import_meta_graph(save_path + '.meta')
         saver.restore(sess, save_path)
 
-        y_pred = tf.get_collection("y_pred")[0]
-        loss = tf.get_collection("loss")[0]
-        accuracy = tf.get_collection("accuracy")[0]
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        y_pred = tf.get_collection('y_pred')[0]
+        accuracy = tf.get_collection('accuracy')[0]
+        loss = tf.get_collection('loss')[0]
+        prediction = sess.run(y_pred, feed_dict={x: X, y: Y})
+        accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
+        loss = sess.run(loss, feed_dict={x: X, y: Y})
 
         return y_pred, loss, accuracy
