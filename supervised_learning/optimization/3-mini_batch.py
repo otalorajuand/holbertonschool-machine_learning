0233167a9860a3e5460c5dc9544cf30559674628
otalorajuand <otalorajuand@gmail.com>
2023-04-26 11:31:38 -0500

    Addition of task 3

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python3
-"""This module contains the function train_mini_batch"""
+"""Script to train a model using mini batch"""
+
+
 import tensorflow as tf
-
-
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
-def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32, epochs=5, load_path="/tmp/model.ckpt", save_path="/tmp/model.ckpt"):
-    
+
 def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
                      batch_size=32, epochs=5, load_path="/tmp/model.ckpt",
                      save_path="/tmp/model.cpkt"):
-    """trains a loaded neural network model using mini-batch gradient descent"""
+    """
+    Function to train a DNN using mini_batch gradient descent algorithm
+    Args:
+        X_train: numpy.ndarray of shape (m, 784)
+                 contains training data
+        Y_train: one-hot numpy.ndarray of shape (m, 10)
+                 contains training labels
+        X_valid: numpy.ndarray of shape (m, 784)
+                 contains validation data
+        Y_valid: one-hot numpy.ndarray of shape (m, 10)
+                 contains validation labels
+        batch_size: type int number of data points in a batch
+        epochs: type int number of times the training should pass
+                through the whole dataset
+        load_path: path from which to load the model
+        save_path: path to where the model should be saved after training
+    Returns: path where the model was saved
+    """
 
     with tf.Session() as sess:
         saver = tf.train.import_meta_graph(load_path + ".meta")
@@ -22,7 +38,7 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
         loss = tf.get_collection("loss")[0]
         train_op = tf.get_collection("train_op")[0]
 
-        m = X_train.shape[0]
+        m = Xtrain.shape[0]
         if (m % batch_size) == 0:
             num_minibatches = int(m / batch_size)
             check = 1
