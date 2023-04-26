ade445edbd1e39cefa34d02c758ec1119c56ac14
otalorajuand <otalorajuand@gmail.com>
2023-04-26 18:07:07 -0500

    Addition of task 13

---
+++ supervised_learning/optimization/13-batch_norm.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This module contains the function batch_norm"""
+import numpy as np
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    """normalizes an unactivated output of a neural network
+    using batch normalization"""
+    miu = np.mean(Z, axis=0)
+    G2 = np.std(Z, axis=0) ** 2
+
+    Z_norm = (Z - miu)/((G2 + epsilon)**0.5)
+    return gamma * Z_norm  + beta
