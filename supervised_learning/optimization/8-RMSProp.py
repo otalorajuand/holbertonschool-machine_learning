c1024aa44d85fb9a0c3e6d1422b3fc9f2f029d6d
otalorajuand <otalorajuand@gmail.com>
2023-04-26 15:28:57 -0500

    Addition of task 8

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -6,6 +6,6 @@ import tensorflow as tf
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
     """that creates the training operation for a neural network in tensorflow
     using the RMSProp optimization algorithm"""
-    return tf.train.RMSPropOptimizer(alpha, 
-                                     decay=beta2, 
+    return tf.train.RMSPropOptimizer(alpha,
+                                     decay=beta2,
                                      epsilon=epsilon).minimize(loss)
