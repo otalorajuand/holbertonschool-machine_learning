b6754d4741e7450bd8adc8ad2ca6ee49286f77c7
otalorajuand <otalorajuand@gmail.com>
2023-04-26 14:57:30 -0500

    Addition of task 5

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -4,6 +4,6 @@
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
     """updates a variable using the gradient descent with momentum optimization algorithm"""
-    vd = beta1*v + (1-beta1)*grad
-    var = var - alpha*vd
+    vd = beta1 * v + (1 - beta1) * grad
+    var = var - alpha * vd
     return var, vd
