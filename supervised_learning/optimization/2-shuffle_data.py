1fcc29b4738a2310926a35f57759125894e80589
otalorajuand <otalorajuand@gmail.com>
2023-04-26 10:38:04 -0500

    Addition of task 2

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -5,6 +5,7 @@ import numpy as np
 
 def shuffle_data(X, Y):
     """shuffles the data points in two matrices the same way"""
-    X_perm = np.random.permutation(X)
-    Y_perm = np.random.permutation(Y)
+    m = np.random.permutation(X.shape[0])
+    X_perm = X[m]
+    Y_perm = Y[m]
     return X_perm, Y_perm
