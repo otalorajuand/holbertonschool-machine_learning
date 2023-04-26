bd164b1155c5a39de13439cab79b4a8f7ea216ff
otalorajuand <otalorajuand@gmail.com>
2023-04-26 17:26:59 -0500

    Addition of task 11

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -4,5 +4,5 @@
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """updates the learning rate using inverse time decay in numpy"""
-    res = (1/(1 + decay_rate * int(global_step / decay_step)))*alpha
-    return res
+    alpha = alpha / (1 + decay_rate * int(global_step / decay_step))
+    return alpha
