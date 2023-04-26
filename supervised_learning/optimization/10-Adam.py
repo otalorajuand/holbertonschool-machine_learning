4df439dbe096acfa01bd306a55d90b674741462f
otalorajuand <otalorajuand@gmail.com>
2023-04-26 16:45:28 -0500

    Addition of task 10

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -6,4 +6,4 @@ import tensorflow as tf
 def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
     """creates the training operation for a neural network in tensorflow
     using the Adam optimization algorithm"""
-    return tf.train.AdamOptimizer(alpha,beta1,beta2,epsilon).minimize(loss)
+    return tf.train.AdamOptimizer(alpha, beta1, beta2, epsilon).minimize(loss)
