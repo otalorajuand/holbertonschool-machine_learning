7b0f71e1c65eecc7947ab148bb30d912b507b59e
otalorajuand <otalorajuand@gmail.com>
2023-04-26 16:28:28 -0500

    Addition of task 9

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -5,9 +5,9 @@
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     """updates a variable in place using the Adam optimization algorithm"""
     vd = beta1 * v + (1 - beta1) * grad
-    s = beta2 * s + (1 - beta2) * (grad ** 2)
+    sd = beta2 * s + (1 - beta2) * (grad ** 2)
 
     vd_corrected = vd / (1 - (beta1 ** t))
-    s_corrected = s / (1 - (beta2 ** t))
+    s_corrected = sd / (1 - (beta2 ** t))
     var = var - alpha * (vd_corrected / ((s_corrected ** 0.5) + epsilon))
     return var, vd_corrected, s_corrected
