eb48687e6c8524230e827d8ae08460ec3e170db2
otalorajuand <otalorajuand@gmail.com>
2023-05-02 16:08:36 -0500

    Addition of task 4

---
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""conducts forward propagation using Dropout"""
+import numpy as np
+
+
+def dropout_forward_prop(X, weights, L, keep_prob):
+    """conducts forward propagation using Dropout
+
+    Params:
+        X: a numpy.ndarray of shape (nx, m) containing the input data
+        for the network
+        weights: a dictionary of the weights and biases of the neural network
+        L: the number of layers in the network
+        keep_prob: the probability that a node will be kept
+
+    Returns: a dictionary containing the outputs of each layer and
+    the dropout mask used on each layer
+    """
+    cache = {}
+    cache['A0'] = X
+
+    for i in range(L):
+        W = weights['W' + str(i + 1)]
+        A = cache['A' + str(i)]
+        B = weights['b' + str(i + 1)]
+
+        D = np.where(np.random.rand(A.shape[0], A.shape[1]) < keep_prob, 1, 0)
+
+        A_drop = np.multiply(A, D)
+
+        Z = np.dot(W, A_drop) + B
+
+        if i == L - 1:
+            soft_max = np.exp(Z)
+            cache['A' + str(i + 1)] = (soft_max / np.sum(soft_max, axis=0,
+                                                         keepdims=True))
+        else:
+            tanh = np.tanh(Z)
+            cache['A' + str(i + 1)] = tanh / keep_prob
+        cache['D' + str(i + 1)] = D
+
+    return cache
