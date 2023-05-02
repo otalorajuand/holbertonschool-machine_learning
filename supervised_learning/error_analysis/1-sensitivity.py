5ff519ee33867cccfa12952abba7c0d9e72721d9
otalorajuand <otalorajuand@gmail.com>
2023-05-01 19:27:59 -0500

    Addition of task 1

---
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""this module contains the function sensitivity"""
+import numpy as np
+
+
+def sensitivity(confusion):
+    """calculates the sensitivity for each class in a confusion matrix
+
+    Params:
+        confusion: a confusion numpy.ndarray of shape (classes, classes)
+        where row indices represent the correct labels and column indices 
+        represent the predicted labels
+
+    Returns: a numpy.ndarray of shape (classes,) containing the sensitivity
+    of each class
+    """
+
+    confusion_diag = np.diag(confusion)
+    confusion_rows_sum = np.sum(confusion, axis=1)
+
+    sensitivity = confusion_diag / confusion_rows_sum
+    return sensitivity
