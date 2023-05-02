673fb8a676123c3088bc069848577a3d0c20aec2
otalorajuand <otalorajuand@gmail.com>
2023-05-02 17:47:03 -0500

    Addition of task 5

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -16,27 +16,27 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
         alpha: the learning rate
         keep_prob: the probability that a node will be kept
         L: the number of layers of the network
-    
+
     Returns: Nothing
     """
 
     m = Y.shape[1]
     dZ = cache["A" + str(L)] - Y
-    
+
     for i in range(L, 0, -1):
-        A_prev = cache["A" + str(i-1)]
+        A_prev = cache["A" + str(i - 1)]
         W = weights["W" + str(i)]
         b = weights["b" + str(i)]
-        
+
         dA = np.dot(W.T, dZ)
         if i > 1:
-            dA *= cache["D" + str(i-1)]
+            dA *= cache["D" + str(i - 1)]
         dA /= keep_prob
-        
-        dW = (1/m) * np.dot(dZ, A_prev.T)
-        db = (1/m) * np.sum(dZ, axis=1, keepdims=True)
-        
+
+        dW = (1 / m) * np.dot(dZ, A_prev.T)
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+
         dZ = dA * (1 - np.power(A_prev, 2))
-        
+
         weights["W" + str(i)] -= alpha * dW
         weights["b" + str(i)] -= alpha * db
