34f21be46ddac2bdc92e8dbfd904992958128cbc
otalorajuand <otalorajuand@gmail.com>
2023-05-01 19:28:41 -0500

    Addition of task 1

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -8,7 +8,7 @@ def sensitivity(confusion):
 
     Params:
         confusion: a confusion numpy.ndarray of shape (classes, classes)
-        where row indices represent the correct labels and column indices 
+        where row indices represent the correct labels and column indices
         represent the predicted labels
 
     Returns: a numpy.ndarray of shape (classes,) containing the sensitivity
