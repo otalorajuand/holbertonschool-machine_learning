31f7d45dfac4dc921e13a256b6af0c6676df8a19
otalorajuand <otalorajuand@gmail.com>
2023-05-02 17:37:15 -0500

    Addition of task 5

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -20,35 +20,22 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     Returns: Nothing
     """
 
-    deltas = {}
-    nl = L
-    weights_copy = weights.copy()
-    deltas['DZ' + str(nl)] = cache['A' + str(nl)] - Y
     m = Y.shape[1]
-    DW = (1 / m) * np.dot(deltas['DZ' + str(nl)], cache['A' +
-                                                        str(nl - 1)].T)
-    DB = (1 / m) * np.sum(deltas['DZ' + str(nl)], axis=1, keepdims=True)
-
-    W = 'W' + str(nl)
-    B = 'b' + str(nl)
-    weights[W] = weights_copy[W] - alpha * DW
-    weights[B] = weights_copy[B] - alpha * DB
-
-    for i in reversed(range(1, nl)):
-
-        W = weights_copy['W' + str(i + 1)]
-        DZ = deltas['DZ' + str(i + 1)]
-
-        A = cache['A' + str(i)]
-        A_1 = cache['A' + str(i - 1)]
-
-        deltas['DZ' + str(i)] = np.matmul(W.T, DZ) * (A * (1 - A))
-        deltas['DZ' + str(i)] /= keep_prob
-        DZ_1 = deltas['DZ' + str(i)]
-        DW = ((1 / m) * np.matmul(A_1, DZ_1.T))
-        DB = (1 / m) * np.sum(DZ_1, axis=1, keepdims=True)
-
-        W = 'W' + str(i)
-        B = 'b' + str(i)
-        weights[W] = weights_copy[W] - (alpha * DW).T
-        weights[B] = weights_copy[B] - alpha * DB
+    dZ = cache["A" + str(L)] - Y
+    
+    for i in range(L, 1, -1):
+        A_prev = cache["A" + str(i-1)]
+        W = weights["W" + str(i)]
+        b = weights["b" + str(i)]
+        
+        dA = np.dot(W.T, dZ)
+        dA *= cache["D" + str(i-1)]
+        dA /= keep_prob
+        
+        dW = (1/m) * np.dot(dZ, A_prev.T)
+        db = (1/m) * np.sum(dZ, axis=1, keepdims=True)
+        
+        dZ = dA * (1 - np.power(A_prev, 2))
+        
+        weights["W" + str(i)] -= alpha * dW
+        weights["b" + str(i)] -= alpha * db
