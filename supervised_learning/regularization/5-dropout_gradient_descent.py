a0cf5e7b59fbe2c5643a25cf0217faa7e5f94be0
otalorajuand <otalorajuand@gmail.com>
2023-05-02 17:45:01 -0500

    Addition of task 5

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -23,13 +23,14 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     m = Y.shape[1]
     dZ = cache["A" + str(L)] - Y
     
-    for i in range(L, 1, -1):
+    for i in range(L, 0, -1):
         A_prev = cache["A" + str(i-1)]
         W = weights["W" + str(i)]
         b = weights["b" + str(i)]
         
         dA = np.dot(W.T, dZ)
-        dA *= cache["D" + str(i-1)]
+        if i > 1:
+            dA *= cache["D" + str(i-1)]
         dA /= keep_prob
         
         dW = (1/m) * np.dot(dZ, A_prev.T)
