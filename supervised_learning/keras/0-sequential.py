1c15211ffbc6038a56d4dad457b7a81ce4cd0475
otalorajuand <otalorajuand@gmail.com>
2023-05-08 11:16:30 -0500

    Addition of task 0

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -26,7 +26,7 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
 
     for i in range(1, len(layers)):
         model.add(K.layers.Dropout(1 - keep_prob))
-        model.add(K.layers.Dense(layers[i], 
+        model.add(K.layers.Dense(layers[i],
                                  activation=activations[i],
                                  kernel_regularizer=L2,
                                  name='dense_' + str(i)))
