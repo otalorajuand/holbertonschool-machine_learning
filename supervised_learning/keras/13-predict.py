64c3dca55d7faa10117915b31b99c51c18a3b97c
otalorajuand <otalorajuand@gmail.com>
2023-05-08 19:46:40 -0500

    Addition of task 13

---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""This module contains the function predict"""
+
+
+def predict(network, data, verbose=False):
+    """makes a prediction using a neural network
+
+    Params:
+        network: the network model to make the prediction with
+        data: the input data to make the prediction with
+        verbose: a boolean that determines if output should be printed during the prediction process
+
+    Returns: the prediction for the data
+    """
+    prediction = network.predict(data, verbose=verbose)
+    return prediction
