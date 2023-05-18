23383bcd2339af6bde8de6091e948dc66f6e8928
otalorajuand <otalorajuand@gmail.com>
2023-05-18 09:15:53 -0500

    Addition of task 2

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -4,60 +4,37 @@ import numpy as np
 
 
 def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
-    """
-    Perform backpropagation over a convolutional layer.
-
-    Arguments:
-    dZ -- numpy.ndarray of shape (m, h_new, w_new, c_new) containing the partial derivatives
-          with respect to the unactivated output of the convolutional layer
-    A_prev -- numpy.ndarray of shape (m, h_prev, w_prev, c_prev) containing the output of
-              the previous layer
-    W -- numpy.ndarray of shape (kh, kw, c_prev, c_new) containing the kernels for the convolution
-    b -- numpy.ndarray of shape (1, 1, 1, c_new) containing the biases applied to the convolution
-    padding -- string, type of padding used: 'same' or 'valid' (default: 'same')
-    stride -- tuple of (sh, sw) containing the strides for the convolution
-              sh is the stride for the height
-              sw is the stride for the width
-
-    Returns:
-    dA_prev -- partial derivatives with respect to the previous layer
-    dW -- partial derivatives with respect to the kernels
-    db -- partial derivatives with respect to the biases
-    """
-    
-    # Retrieve dimensions
-    m, h_prev, w_prev, c_prev = A_prev.shape
     m, h_new, w_new, c_new = dZ.shape
-    kh, kw, c_prev, c_new = W.shape
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw, _, _ = W.shape
     sh, sw = stride
-    
-    # Initialize output gradients
-    dA_prev = np.zeros_like(A_prev)
-    dW = np.zeros_like(W)
-    db = np.zeros_like(b)
-    
-    # Apply padding to dA_prev based on the padding type
+
     if padding == "same":
         pad_h = int(np.ceil(((h_prev - 1) * sh + kh - h_prev) / 2))
         pad_w = int(np.ceil(((w_prev - 1) * sw + kw - w_prev) / 2))
-        dA_prev = np.pad(dA_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)), mode='constant')
-    elif padding == "valid":
-        pad_h = 0
-        pad_w = 0
-    
-    # Backpropagation loop
+    else:
+        pad_h, pad_w = 0, 0
+
+    A_prev_pad = np.pad(A_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)), mode='constant')
+
+    dA_prev_pad = np.zeros_like(A_prev_pad)
+    dW = np.zeros_like(W)
+    db = np.zeros_like(b)
+
     for i in range(m):
         for h in range(h_new):
             for w in range(w_new):
                 for c in range(c_new):
-                    # Compute gradients for dA_prev, dW, db
-                    dA_prev[i, h * sh:h * sh + kh, w * sw:w * sw + kw, :] += np.sum(
-                        W[:, :, :, c] * dZ[i, h, w, c], axis=-1)
-                    dW[:, :, :, c] += A_prev[i, h * sh:h * sh + kh, w * sw:w * sw + kw, :] * dZ[i, h, w, c]
+                    vert_start = h * sh
+                    vert_end = vert_start + kh
+                    horiz_start = w * sw
+                    horiz_end = horiz_start + kw
+
+                    a_slice = A_prev_pad[i, vert_start:vert_end, horiz_start:horiz_end, :]
+                    dA_prev_pad[i, vert_start:vert_end, horiz_start:horiz_end, :] += W[:, :, :, c] * dZ[i, h, w, c]
+                    dW[:, :, :, c] += a_slice * dZ[i, h, w, c]
                     db[:, :, :, c] += dZ[i, h, w, c]
-    
-    # Adjust dA_prev if padding is used
-    if padding == "same":
-        dA_prev = dA_prev[:, pad_h:-pad_h, pad_w:-pad_w, :]
-    
+
+    dA_prev = dA_prev_pad[:, pad_h:h_prev+pad_h, pad_w:w_prev+pad_w, :]
+
     return dA_prev, dW, db
