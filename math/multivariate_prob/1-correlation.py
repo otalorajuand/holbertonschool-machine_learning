96f7869821e0ad76b5cccaf91e1183194f510548
otalorajuand <otalorajuand@gmail.com>
2023-06-29 15:00:37 -0500

    Addition of task 1

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -16,7 +16,7 @@ def correlation(C):
     if not isinstance(C, np.ndarray):
         raise TypeError('C must be a numpy.ndarray')
 
-    if C.shape[0] != C.shape[1]:
+    if len(C.shape) < 2 or C.shape[0] != C.shape[1]:
         raise ValueError('C must be a 2D square matrix')
 
     cor = C / np.sqrt(np.outer(np.diag(C), np.diag(C)))
