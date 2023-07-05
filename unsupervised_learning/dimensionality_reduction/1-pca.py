3b1c12e50c617059ee4cd31371ee7a5484531754
otalorajuand <otalorajuand@gmail.com>
2023-07-05 14:46:54 -0500

    Addition of task 1

---
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""This module contains the function pca"""
+import numpy as np
+
+
+def pca(X, ndim):
+    """performs PCA on a dataset
+
+    Params:
+        X is a numpy.ndarray of shape (n, d) where:
+            n is the number of data points
+            d is the number of dimensions in each point
+        ndim is the new dimensionality of the transformed X
+
+    Returns: T, a numpy.ndarray of shape (n, ndim) containing
+             the transformed version of X
+    """
+
+    # U singular_v, Sigma singular_v, Vh right singular_v
+    X_mean = X - np.mean(X, axis=0)
+    u, Sigma, vh = np.linalg.svd(X_mean)
+    w = vh.T
+    wr = w[:, :ndim]
+
+    return np.matmul(X, wr)
