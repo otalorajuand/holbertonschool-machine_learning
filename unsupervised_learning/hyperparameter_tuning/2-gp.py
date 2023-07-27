1afee53c50c0f843533c0e68d736d5477853cb47
otalorajuand <otalorajuand@gmail.com>
2023-07-27 14:53:27 -0500

    Addition of task 5

--- unsupervised_learning/hyperparameter_tuning/2-gp.py
+++ unsupervised_learning/hyperparameter_tuning/2-gp.py
@@ -69,8 +69,8 @@ class GaussianProcess:
 
         X_new is a numpy.ndarray of shape (1,) that represents the new sample
         point
-        Y_new is a numpy.ndarray of shape (1,) that represents the new sample function
-        value
+        Y_new is a numpy.ndarray of shape (1,) that represents the new sample
+        function value
         """
         self.X = np.append(self.X, X_new).reshape(-1, 1)
         self.Y = np.append(self.Y, Y_new).reshape(-1, 1)
