09d17ea535a07f544988e085a4b31f0178f285ac
otalorajuand <otalorajuand@gmail.com>
2023-08-22 14:05:44 -0500

    Addition of task 5

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -3,7 +3,16 @@
 from gensim.models import FastText
 
 
-def fasttext_model(sentences, size=100, min_count=5, negative=5, window=5, cbow=True, iterations=5, seed=0, workers=1):
+def fasttext_model(
+        sentences,
+        size=100,
+        min_count=5,
+        negative=5,
+        window=5,
+        cbow=True,
+        iterations=5,
+        seed=0,
+        workers=1):
     """creates and trains a genism fastText model
 
     Args:
