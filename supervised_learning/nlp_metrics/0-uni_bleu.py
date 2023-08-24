57fb3b316e8f69f82cdf4238f688090e2c1809d1
otalorajuand <otalorajuand@gmail.com>
2023-08-24 12:15:13 -0500

    Addition of task 0

---
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""This module contains the function uni_bleu"""
+import numpy as np
+
+
+def uni_bleu(references, sentence):
+    """
+    sentence_blue
+    Args:
+        references: list of reference translations
+        sentence: list containing the model proposed sentence
+    Returns: unigram BLEU score
+    """
+
+    len_sen = len(sentence)
+
+    word_references = list(
+        set([x for reference in references for x in reference]))
+
+    counts = 0
+    for word in sentence:
+        if word in word_references:
+            counts += 1
+
+    min_reference = min([len(reference) for reference in references])
+    bp = np.exp(1 - (min_reference / len_sen))
+
+    return bp * (counts / len_sen)
