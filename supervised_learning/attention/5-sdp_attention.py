3f4c59846c4474af356c6e304e9c88146851053f
otalorajuand <otalorajuand@gmail.com>
2023-08-29 15:31:56 -0500

    Addition of task 5

--- supervised_learning/attention/5-sdp_attention.py
+++ supervised_learning/attention/5-sdp_attention.py
@@ -19,8 +19,8 @@ def sdp_attention(Q, K, V, mask=None):
     Returns: output, weights
     - outputa tensor with its last two dimensions as (..., seq_len_q, dv)
       containing the scaled dot product attention
-    - weights a tensor with its last two dimensions as (..., seq_len_q, seq_len_v)
-      containing the attention weights
+    - weights a tensor with its last two dimensions as 
+      (..., seq_len_q, seq_len_v) containing the attention weights
     """
     seq_len_q, dk = tf.shape(Q)[-2], float(tf.shape(Q)[-1])
     seq_len_v = tf.shape(K)[-2]
