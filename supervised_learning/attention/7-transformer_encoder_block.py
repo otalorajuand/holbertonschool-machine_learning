79ecc43e63774d67724ddf08c783638ff7ff45d7
otalorajuand <otalorajuand@gmail.com>
2023-08-30 10:19:48 -0500

    Addition of task 7

--- supervised_learning/attention/7-transformer_encoder_block.py
+++ supervised_learning/attention/7-transformer_encoder_block.py
@@ -19,7 +19,7 @@ class EncoderBlock(tf.keras.layers.Layer):
         super().__init__()
 
         self.mha = MultiHeadAttention(dm, h)
-        self.dense_hidden = tf.keras.layers.Dense(units=hidden, activation='relu')
+        self.dense_hidden = tf.keras.layers.Dense(hidden, activation='relu')
         self.dense_output = tf.keras.layers.Dense(units=dm)
         self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
         self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
