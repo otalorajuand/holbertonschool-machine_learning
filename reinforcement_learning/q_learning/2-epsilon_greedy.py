bec91051e4f5fbde866272cbe4899d0af48b3737
otalorajuand <otalorajuand@gmail.com>
2023-10-04 14:18:47 -0500

    Addition of task 2

---
+++ reinforcement_learning/q_learning/2-epsilon_greedy.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def epsilon_greedy(Q, state, epsilon):
+    """uses epsilon-greedy to determine the next action
+
+    Args:
+    - Q is a numpy.ndarray containing the q-table
+    - state is the current state
+    - epsilon is the epsilon to use for the calculation
+
+    Returns: the next action index
+    """
+
+    p = np.random.uniform()
+
+    if p > epsilon:
+        next_action = np.argmax(Q[state, :])
+    else:
+        next_action = np.random.randint(Q.shape[1])
+
+    return next_action
