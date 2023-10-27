b2403a578709175e87e10a5479d4287c66c593c6
otalorajuand <otalorajuand@gmail.com>
2023-10-27 16:14:02 -0500

    Addition of train file

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -9,3 +9,26 @@ def policy(matrix, weight):
 
     x = matrix @ weight
     return np.exp(x) / np.exp(x).sum()
+
+
+def policy_gradient(state, weight):
+    """computes the Monte-Carlo policy gradient based on a state
+       and a weight matrix
+
+    Args:
+    - state: matrix representing the current observation of the environment
+    - weight: matrix of random weight
+
+    Return: the action and the gradient (in this order)
+    """
+    probs = policy(state, weight)
+    
+    # Choose an action randomly based on the probabilities
+    action = np.random.choice([0, 1], p=probs[0])
+    
+    # Compute the gradient of the log-probability of the chosen action
+    grad = state.T - probs
+    grad = grad * (1 - probs)
+    grad = grad[action]
+
+    return action, grad
