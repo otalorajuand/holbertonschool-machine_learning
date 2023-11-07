8052cf648760b49141e71976a6660265dccc3b9e
otalorajuand <otalorajuand@gmail.com>
2023-11-07 11:16:05 -0500

    Addition of task 1

---
+++ pipeline/apis/1-sentience.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""This module includes the function sentientPlanets"""
+import requests
+
+
+def sentientPlanets():
+    """Returns the list of names of the home planets of all sentient species
+    """
+
+    url = 'https://swapi-api.hbtn.io/api/species'
+    r = requests.get(url)
+    next_ = r.json().get('next')
+
+    planets = []
+    while True:
+
+        lista = r.json().get("results")
+
+        print(len(lista))
+
+        for species in lista:
+            homeworld = species.get('homeworld')
+
+            if homeworld:
+                planet_request = requests.get(homeworld)
+                if species.get('classification') == 'sentient' or species.get(
+                        'designation') == 'sentient':
+                    planets.append(planet_request.json().get('name'))
+
+        if next_ is None:
+            break
+
+        r = requests.get(next_)
+        next_ = r.json().get('next')
+
+    return planets
