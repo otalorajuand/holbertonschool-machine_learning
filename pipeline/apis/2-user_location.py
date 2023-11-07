54e5500bf6a75e914754e3694e518f180528e7ae
otalorajuand <otalorajuand@gmail.com>
2023-11-07 15:01:06 -0500

    Addition of task 2

---
+++ pipeline/apis/2-user_location.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""This module includes the script that retrieves github info"""
+import requests
+import sys
+import time
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        raise ValueError('Incorrect number of input parameters')
+
+    r = requests.get(sys.argv[-1])
+    r_status_code = r.status_code
+
+    if r_status_code == 200:
+        print(r.json().get('location'))
+    elif r_status_code == 403:
+        rate_limit = r.headers.get('X-Ratelimit-Reset')
+        time_minutes = round((int(rate_limit) - time.time()) / 60)
+        print(f'Reset in {time_minutes} min')
+    elif r.status_code == 404:
+        print('Not found')
