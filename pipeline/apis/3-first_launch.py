781b67bb3257dbcdfcd7a0075d37d77cf660b078
otalorajuand <otalorajuand@gmail.com>
2023-11-08 11:59:32 -0500

    Addition of task 3

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -3,30 +3,32 @@
 import requests
 
 if __name__ == '__main__':
-    launches_request = requests.get('https://api.spacexdata.com/v5/launches/')
+    launches_request = requests.get(
+        'https://api.spacexdata.com/v4/launches/upcoming')
 
     launches_request_json = launches_request.json()
 
-    lastest_launch_date = 0
-    lastes_launch = {}
+    first_launch_date = float('inf')
+    first_launch = {}
     for launch in launches_request_json:
-        if launch.get('date_unix') > lastest_launch_date:
-            lastes_request_date = launch.get('date_unix')
-            lastes_launch = launch
+        if launch.get('date_unix') < first_launch_date:
+            first_launch_date = int(launch.get('date_unix'))
+            first_launch = launch
+            print(first_launch_date)
 
     # launch
-    launch_name = lastes_launch.get('name')
-    date = lastes_launch.get('date_local')
+    launch_name = first_launch.get('name')
+    date = first_launch.get('date_local')
 
     # rocket info
-    rocket_id = lastes_launch.get('rocket')
+    rocket_id = first_launch.get('rocket')
     rocket_request = requests.get(
         'https://api.spacexdata.com/v4/rockets/' +
         rocket_id)
     rocker_name = rocket_request.json().get('name')
 
     # launchpad info
-    launch_pad_id = lastes_launch.get('launchpad')
+    launch_pad_id = first_launch.get('launchpad')
     launch_pad_request = requests.get(
         'https://api.spacexdata.com/v4/launchpads/' +
         launch_pad_id)
