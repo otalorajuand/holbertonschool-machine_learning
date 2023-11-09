38bae156933beb472cc5e436905fe29e92e10b50
otalorajuand <otalorajuand@gmail.com>
2023-11-09 17:22:33 -0500

    Addition of task 10

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Write a script that lists all genres from hbtn_0d_tvshows and displays the
 -- number of shows linked to each.
-SELECT t1.name as genre, count(t2.show_id) AS number_of_shows
+SELECT t1.name AS genre, count(t2.show_id) AS number_of_shows
 FROM tv_genres t1
 INNER JOIN tv_show_genres t2
 ON t1.id = t2.genre_id
