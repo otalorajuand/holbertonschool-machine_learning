e0f7b648835d3326fed4ddd6e5e576b09c3a5324
otalorajuand <otalorajuand@gmail.com>
2023-11-09 17:10:53 -0500

    Addition of task 8

--- pipeline/databases/8-genre_id_by_show.sql
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 -- Write a script that lists all shows contained in hbtn_0d_tvshows that
 -- have at least one genre linked.
-select t1.title, t2.genre_id
-from tv_shows t1
-inner join tv_show_genres t2
-on t1.id = t2.show_id
-order by t1.title ASC, t2.genre_id
+SELECT t1.title, t2.genre_id
+FROM tv_shows t1
+INNER JOIN tv_show_genres t2
+ON t1.id = t2.show_id
+ORDER BY t1.title ASC, t2.genre_id ASC
