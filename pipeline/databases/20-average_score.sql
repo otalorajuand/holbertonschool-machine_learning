378f66581b858b3e81fda4ea126699f20e73e94e
otalorajuand <otalorajuand@gmail.com>
2023-11-10 14:46:01 -0500

    Addition of task 20

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -9,10 +9,10 @@ BEGIN
 
     SELECT AVG(score) INTO avg_score
     FROM corrections 
-    WHERE user_id = user_id;
+    WHERE corrections.user_id = user_id;
 
     UPDATE users
     SET average_score = avg_score
-    WHERE id = user_id;
+    WHERE users.id = user_id;
 END //
 DELIMITER ;
