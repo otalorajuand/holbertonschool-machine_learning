02f7a95036b83afa5ad14cf883eeeb07c55597da
otalorajuand <otalorajuand@gmail.com>
2023-11-14 17:37:08 -0500

    Addition of task 34

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -3,29 +3,32 @@
    logs stored in MongoDB"""
 from pymongo import MongoClient
 
+if __name__ == "__main__":
+    """
+    Gets stats about Nginx logs stored in MongoDB
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
 
-client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+    count_documents = len(list(collection.find()))
+    print('{} logs'.format(count_documents))
 
-collection = client.logs.nginx
-count_documents = len(list(collection.find()))
-print('{} logs'.format(count_documents))
+    print('Methods:')
 
-print('Methods:')
+    n_get = len(list(collection.find({"method": "GET"})))
+    print('\tmethod GET: {}'.format(n_get))
 
-n_get = len(list(collection.find({"method": "GET"})))
-print('\tmethod GET: {}'.format(n_get))
+    n_post = len(list(collection.find({"method": "POST"})))
+    print('\tmethod POST: {}'.format(n_post))
 
-n_post = len(list(collection.find({"method": "POST"})))
-print('\tmethod POST: {}'.format(n_post))
+    n_put = len(list(collection.find({"method": "PUT"})))
+    print('\tmethod PUT: {}'.format(n_put))
 
-n_put = len(list(collection.find({"method": "PUT"})))
-print('\tmethod PUT: {}'.format(n_put))
+    n_patch = len(list(collection.find({"method": "PATCH"})))
+    print('\tmethod PATCH: {}'.format(n_patch))
 
-n_patch = len(list(collection.find({"method": "PATCH"})))
-print('\tmethod PATCH: {}'.format(n_patch))
+    n_delete = len(list(collection.find({"method": "DELETE"})))
+    print('\tmethod DELETE: {}'.format(n_delete))
 
-n_delete = len(list(collection.find({"method": "DELETE"})))
-print('\tmethod DELETE: {}'.format(n_delete))
-
-n_status = len(list(collection.find({"method": "DELETE", "path": "/status"})))
-print('{} status check'.format(n_status))
+    n_status = len(list(collection.find({"method": "DELETE", "path": "/status"})))
+    print('{} status check'.format(n_status))
