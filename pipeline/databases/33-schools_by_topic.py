cffc876c2bc8a1b073f3f5be835f6c195d967a2b
otalorajuand <otalorajuand@gmail.com>
2023-11-14 16:09:46 -0500

    Addition of task 33

---
+++ pipeline/databases/33-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This module contains the function schools_by_topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """returns the list of school having a specific topic
+
+    Args:
+    - mongo_collection will be the pymongo collection object
+    - topic (string) will be topic searched
+    """
+    result = mongo_collection.find({topics: topic})
+    return list(result)
